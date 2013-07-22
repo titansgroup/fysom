4321360b000a59fd5cf556d82454fa1529ea490e
Felipe Prenholato <philipe.rp@gmail.com>
2013-07-22 16:01:49 -0300

    Add goto method.

--- tests/states.py
+++ tests/states.py
@@ -71,3 +71,36 @@ class StatesTestCase(unittest.TestCase):
         })
 
         self.assertRaises(NoPossibilityFound, fsm.next)
+
+    def test_goto(self):
+        fsm = Fysom({
+            'initial': 'green',
+            'events': [
+                {'name': 'warn', 'src': 'green', 'dst': 'yellow'},
+                {'name': 'panic', 'src': 'yellow', 'dst': 'red'},
+                {'name': 'panic', 'src': 'green', 'dst': 'red'},
+                {'name': 'calm', 'src': 'red', 'dst': 'yellow'},
+                {'name': 'clear', 'src': 'yellow', 'dst': 'green'}
+            ]
+        })
+        self.assertRaises(MultiplePossibilitesFound, fsm.next)
+        self.assertEqual('red', fsm.goto('panic'))
+
+    def test_no_possibility_for_goto(self):
+        fsm = Fysom({
+            'initial': 'green',
+            'events': [
+                {'name': 'warn', 'src': 'green', 'dst': 'yellow'},
+                {'name': 'panic', 'src': 'yellow', 'dst': 'red'},
+                {'name': 'panic', 'src': 'green', 'dst': 'red'},
+                # when call panic from green, we go to red, from red we
+                # should go to yellow, but it is commented and we should
+                # have a exception
+                # {'name': 'calm', 'src': 'red', 'dst': 'yellow'},
+                {'name': 'clear', 'src': 'yellow', 'dst': 'green'}
+            ]
+        })
+
+        self.assertRaises(MultiplePossibilitesFound, fsm.next)
+        self.assertEqual('red', fsm.goto('panic'))
+        self.assertRaises(NoPossibilityFound, fsm.goto, 'calm')
